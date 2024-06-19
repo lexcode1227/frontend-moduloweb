602cf4a6e27ff4064ff125661ee92d35d843089d
Henry Agustin <henryagustin297@gmail.com>
2024-06-19 01:08:55 -0600

    Carpeta corregida correctamente

---
+++ Backend-/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'views')));
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+.then(() => {
+  console.log('Conectado a MongoDB');
+}).catch((err) => {
+  console.error('Error conectando a MongoDB', err);
+});
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}, visita: http://localhost:${PORT}`);
+});
