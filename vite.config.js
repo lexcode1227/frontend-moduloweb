79f75ed58d773ba2274e4fbf4d1c2f2ca096aaaa
Henry Agustin <henryagustin297@gmail.com>
2024-06-22 00:36:17 -0600

    Solucionando problema de SPA's con React y Vite.js cuando se utilizan en conjunto con React Router

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
